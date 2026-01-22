0bf5601bf8080572d6d16fdcb248e7f8b66d9a33
Arif Abbas <abbasarif647@gmail.com>
2026-01-22 10:09:20 +0530

    Interface Segregation principles

--- DesignPattern/Singleton.java
+++ DesignPattern/Singleton.java
@@ -8,18 +8,23 @@ class DatabaseConnection {
         System.out.println("Singleton is Instantiated.");
     }
 
-    public static DatabaseConnection getInstance() {
+   synchronized public static DatabaseConnection getInstance() {
         if (instance == null) {
             instance = new DatabaseConnection();
         }
         return instance;
     }
-
-    
 }
 public class Singleton {
     public static void main(String[] args) {
-        DatabaseConnection.getInstance();
+       DatabaseConnection db1 = DatabaseConnection.getInstance();
+       DatabaseConnection db2 = DatabaseConnection.getInstance();
+
+       if (db1 == db2) {
+           System.out.println("Both references point to the same instance.");
+       } else {
+           System.out.println("Different instances exist.");
+       }
          
     }
 }
